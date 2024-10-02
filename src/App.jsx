427a1f1d7fb2ee7b74723443ddd3788c25574dfc
jdaniera <jasminejasmine14@hotmail.com>
2024-10-01 22:50:19 -0700

    changing fonts and adding background

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   return (
     <div className="App">
       <h1 className='page-title'>Your Own Curated Vancouver Newsfeed</h1>
+      <hr className='divider'/>
+      <p className='date'>Fall 2024</p>
       <Newslist stories={stories} />
     </div>
   );
